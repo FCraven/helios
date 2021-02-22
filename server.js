964a6cb2987ea35742d3ea2627b8d8c90c7d560d
FCraven <craven.francis@gmail.com>
2021-02-22 17:57:30 -0500

    added dotenv for local config vars and a slash route to serve index html

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 const express = require('express');
 const app = express();
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
+const path = require('path')
 const morgan = require('morgan');
 const bodyParser = require('body-parser')
 
 
-
+//middleware
+//static server
+app.use(express.static('public'))
+//logging
 app.use(morgan('tiny'));
+//application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}))
+//application/json
 app.use(bodyParser.json())
 
-
-
-
-
-
+app.get('/', (req,res)=> {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
 
 app.listen(PORT, ()=> {
     console.log(`Server listening on https://localhost:${PORT}`)
