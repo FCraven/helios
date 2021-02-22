56beb124c570da39f5476c235ecb77c8c07a6e1f
FCraven <craven.francis@gmail.com>
2021-02-22 12:52:07 -0500

    geolocation added to get user latitude and longitude

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
-window.onload =()=> {
+'use strict'
 
+window.onload =async ()=> {
     const testHeader = document.createElement('h1')
     const root = document.getElementById('root')
 
     testHeader.textContent = ' Hello World '
     testHeader.classList.add('test-header')
     root.appendChild(testHeader)
+
+
+
+const getWeather =async ()=> {
+
+    try {
+
+        function success(position) {
+            const lat = position.coords.latitude;
+            const lon= position.coords.longitude;
+            console.log('lat',lat)
+            console.log('lon', lon)
+
+          }
+
+          function error() {
+            console.log('Unable to retrieve your location');
+          }
+
+        if(!navigator.geolocation) {
+            console.log('Geolocation is not supported by your browser');
+          } else {
+            console.log('Locating…');
+            await navigator.geolocation.getCurrentPosition(success, error);
+          }
+
+    } catch(err) {
+        console.log(
+            'Could not get coordinates from browser.'
+        )
+    }
+
+}
+
+await getWeather()
+
+
 }
