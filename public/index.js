ab902cc1a746cedf6a2c41747f32ccdb07ab069f
FCraven <craven.francis@gmail.com>
2021-02-23 14:38:31 -0500

    save response to forecast request to localStorage

--- public/index.js
+++ public/index.js
@@ -1,49 +1,40 @@
 'use strict'
 
 window.onload =async ()=> {
-    const testHeader = document.createElement('h1')
-    const root = document.getElementById('root')
 
-    testHeader.textContent = ' Hello World '
-    testHeader.classList.add('test-header')
-    root.appendChild(testHeader)
+  const getWeather =async ()=> {
+    const ls = window.localStorage;
+      try {
+          const success =async (position)=> {
+              const { latitude, longitude } = position.coords;
+              const queryUrl = `/api/weather?lat=${latitude}&lon=${longitude}`;
+              const response = await fetch(queryUrl);
+              const data = await response.json();
+              localStorage.setItem('forecast', JSON.stringify(data));
+              console.log('Fetch data ==-->', data);
+            }
 
+          const error =()=> {
+              //do something here that makes a nice landing screen and prompts user to search for weather
+              //UNABLE TO GET COORDINATES FROM DEVICE
 
+              console.log('Unable to retrieve your location');
+            }
 
-const getWeather =async ()=> {
+          if(!navigator.geolocation) {
+            //do somthing here to handle for no geolocation
+              console.log('Geolocation is not supported by your browser');
+            } else {
+                console.log('Locating…');
+                await navigator.geolocation.getCurrentPosition(success, error);
+            }
+      } catch(err) {
+          console.log(
+              'Could not get coordinates from browser.'
+          )
+      }
+  }
 
-    try {
-
-        function success(position) {
-            const lat = position.coords.latitude;
-            const lon= position.coords.longitude;
-            console.log('lat',lat)
-            console.log('lon', lon)
-            fetch(`/api/weather?lat=${lat}&lon=${lon}`)
-            .then(res => res.json())
-            .then(data => console.log(data))
-          }
-
-          function error() {
-            //do something here that makes a nice landing screen and prompts user to search for weather
-            console.log('Unable to retrieve your location');
-          }
-
-        if(!navigator.geolocation) {
-          //do somthing here to handle for no geolocation
-            console.log('Geolocation is not supported by your browser');
-          } else {
-            console.log('Locating…');
-            await navigator.geolocation.getCurrentPosition(success, error);
-          }
-    } catch(err) {
-        console.log(
-            'Could not get coordinates from browser.'
-        )
-    }
-
-}
-
-await getWeather()
+  await getWeather()
 
 }
