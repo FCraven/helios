ae2d3c6f8268d248da84dcf5c96eb4b53b76c134
FCraven <craven.francis@gmail.com>
2021-02-23 22:15:17 -0500

    set response to forecast object / next use data to hydrate dom

--- public/index.js
+++ public/index.js
@@ -10,6 +10,7 @@ const loadingSection = document.getElementById('loading-section')
 window.onload =()=> {
   const success = async (position)=> {
       //Dependencies
+      let forecast;
       const { latitude, longitude } = position.coords;
       const iconsObject = {
         'clear-day': '',
@@ -24,16 +25,18 @@ window.onload =()=> {
         'partly-cloudy-night': ''
       }
 
+      //FETCH FORECAST BASED ON USER LOCATION
       try{
         const queryUrl = `/api/weather?lat=${latitude}&lon=${longitude}`;
         const response = await fetch(queryUrl);
         const data = await response.json();
+        forecast = data;
         console.log('fetched data--> ', data)
         } catch(error) {
-          console.log('Error fetching weather data')
+            console.log('Error fetching weather data')
         }
 
-
+        console.log('data after try/catch block -->> ', forecast)
 
       //HIDE SPINNER
       loadingSection.style.display = 'none';
