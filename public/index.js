904ecbe64d2a6b031197d486949d5844f28f2953
FCraven <craven.francis@gmail.com>
2021-03-05 00:25:31 -0500

    working through switch to cover cases in keys of current weather object. implemented function to determine human readable direction from wind bearing

--- public/index.js
+++ public/index.js
@@ -28,6 +28,43 @@ window.onload =()=> {
         'tornado' : './svg/tornado.svg'
       }
 
+      const determineBearing =(bearing)=> {
+        const b = bearing
+        if(b > 348.75 && b <= 360 || b >=0 && b <= 11.25){
+            return 'North'
+        } else if(b > 11.25 && b <= 33.75){
+            return 'NNE'
+        } else if(b > 33.75 && b <= 56.25){
+            return 'NE'
+        } else if(b > 56.25 && b <= 78.75){
+            return 'ENE'
+        } else if(b > 78.75 && b <= 101.25) {
+            return 'East'
+        } else if(b > 101.25 && b <= 123.75){
+            return 'ESE'
+        } else if(b > 123.75 && b <= 146.25){
+          return 'SE'
+        } else if(b > 146.25 && b <= 168.75){
+          return 'SSE'
+        } else if(b > 168.75 && b <= 191.25){
+          return 'South'
+        } else if(b > 191.25 && b <= 213.75){
+          return 'SSW'
+        } else if(b > 213.75 && b <= 236.25){
+          return 'SW'
+        } else if(b > 236.25 && b <= 258.75){
+          return 'WSW'
+        } else if(b > 258.75 && b <= 281.25){
+          return 'West'
+        } else if(b > 281.25 && b <= 303.75){
+          return 'WNW'
+        } else if(b > 303.75 && b <= 326.25){
+          return 'NW'
+        } else if(b > 326.25 && b <= 348.75){
+          return 'NNW'
+        }
+      }
+
       //FETCH FORECAST BASED ON USER LOCATION
       try{
           const queryUrl = `/api/weather?lat=${latitude}&lon=${longitude}&units=auto`;
@@ -46,53 +83,111 @@ window.onload =()=> {
       const createCurrentWeather =()=> {
 
         const rootDiv = document.getElementById('root')
+        const currentWeather = forecast.currently;
 
-          const { apparentTemperature: feelsLike, cloudCover, dewPoint,
-                  humidity, icon, nearestStormBearing, nearestStormDistance,
-                  ozone, precipIntensity, precipProbability, pressure, summary,
-                  temperature, time, uvIndex, visibility, windBearing,
-                  windGust, windSpeed } = forecast.currently
+        //TOP PORTION WITH CURRENT WEATHER INFO
+        const topWeatherContainer = document.createElement('section')
+              topWeatherContainer.setAttribute('id','top-weather-container')
+              topWeatherContainer.classList.add('flex', 'justify-evenly', 'align-center')
 
+        //ICON
+        const { icon } = currentWeather
+        const img = document.createElement('img');
+              img.setAttribute('src', iconsObject[icon]);
+              img.setAttribute('id','weather-icon')
 
+        const bottomWeatherContainer = document.createElement('section')
+              bottomWeatherContainer.setAttribute('id','bottom-weather-container')
+              bottomWeatherContainer.classList.add('flex', 'justify-center', 'align-center')
 
+        Object.keys(currentWeather).forEach(el => {
+          //WEATHER DATA CONTAINER
+          let dataContainer = document.createElement('article');
+              dataContainer.classList.add('weather-data', 'flex', 'justify-between', 'align-center', 'flex-auto');
 
-                  //TOP PORTION WITH CURRENT WEATHER INFO
-                  const topWeatherContainer = document.createElement('section')
-                  topWeatherContainer.setAttribute('id','top-weather-container')
-                  topWeatherContainer.classList.add('flex', 'justify-evenly', 'align-center')
+          //Title
+          let dataTitle = document.createElement('span');
+              dataTitle.classList.add('data-title', 'flex-auto');
 
-                  //ICON
-                  const img = document.createElement('img');
-                  const mainIcon = forecast.currently.icon;
-                  img.setAttribute('src', iconsObject[mainIcon]);
-                  img.setAttribute('id','weather-icon')
+          //value
+          let dataValue = document.createElement('p');
+              dataValue.classList.add('data-value','flex','flex-auto', 'justify-center', 'align-center');
 
+          switch(el){
 
+            case 'icon':
+              break;
 
-                  const bottomWeatherContainer = document.createElement('section')
-                  bottomWeatherContainer.setAttribute('id','bottom-weather-container')
-                  bottomWeatherContainer.classList.add('flex', 'justify-center', 'align-center')
+            case 'nearestStormBearing':
+              break;
 
-                  Object.keys(forecast.currently).forEach(el => {
+            case 'windBearing':
 
-                    const dataContainer = document.createElement('article');
-                    dataContainer.classList.add('weather-data', 'flex', 'justify-between', 'align-center', 'flex-auto');
+              dataTitle.textContent = 'wind bearing';
+              dataValue.textContent = determineBearing(currentWeather[el])
+              dataContainer.appendChild(dataTitle);
+              dataContainer.appendChild(dataValue);
+              bottomWeatherContainer.appendChild(dataContainer);
+              break;
 
-                    //create div for key and a separate div for value
-                    const dataTitle = document.createElement('span');
-                    dataTitle.textContent = `${el}`;
+            case 'temperature':
 
-                    dataTitle.classList.add('data-title', 'flex-center');
+              dataTitle.textContent = 'temperature';
+              dataValue.innerHTML = `${forecast.currently[el]} &#8457;`;
+              dataContainer.appendChild(dataTitle);
+              dataContainer.appendChild(dataValue);
+              bottomWeatherContainer.appendChild(dataContainer);
+              break;
 
+            case 'nearestStormDistance':
 
-                    const dataValue = document.createElement('p');
-                    dataValue.textContent = `${forecast.currently[el]}`;
-                    dataValue.classList.add('data-value','flex', 'justify-center', 'align-center');
+              const stormBearing = determineBearing(currentWeather['nearestStormBearing']);
+              dataTitle.textContent = 'nearest storm'
+              dataValue.textContent = `${currentWeather[el]} miles ${stormBearing}`
+              dataContainer.appendChild(dataTitle);
+              dataContainer.appendChild(dataValue);
+              bottomWeatherContainer.appendChild(dataContainer);
+              break;
 
-                    dataContainer.appendChild(dataTitle);
-                    dataContainer.appendChild(dataValue);
-                    bottomWeatherContainer.appendChild(dataContainer);
-                  })
+            case 'time':
+
+              let date = new Date(currentWeather[el] * 1000)
+              let slicedDate = date.toString().split(' ').slice(0,4).join(',')
+              dataTitle.textContent = `${el}`
+              dataValue.textContent =`${slicedDate}`
+              dataContainer.appendChild(dataTitle);
+              dataContainer.appendChild(dataValue);
+              bottomWeatherContainer.appendChild(dataContainer);
+              break;
+
+            case 'precipProbability':
+
+              dataTitle.textContent = 'Chance of Rain'
+              dataValue.textContent = `${currentWeather[el]} %`
+              dataContainer.appendChild(dataTitle);
+              dataContainer.appendChild(dataValue);
+              bottomWeatherContainer.appendChild(dataContainer);
+              break;
+
+            case 'apparentTemperature':
+
+              dataTitle.textContent = 'Feels Like: ';
+              dataValue.innerHTML = `${forecast.currently[el]} &#8457;`;
+              dataContainer.appendChild(dataTitle);
+              dataContainer.appendChild(dataValue);
+              bottomWeatherContainer.appendChild(dataContainer);
+              break;
+
+            default:
+
+              dataTitle.textContent = `${el}`
+              dataValue.textContent = `${forecast.currently[el]}`;
+              dataContainer.appendChild(dataTitle);
+              dataContainer.appendChild(dataValue);
+              bottomWeatherContainer.appendChild(dataContainer);
+          }
+
+        })
 
                   topWeatherContainer.appendChild(img)
 
